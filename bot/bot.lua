2427be385ea81e3ec0ab6e6bdfc51140066ffbab
milad44100 <miladdehghan44111@gmail.com>
2016-10-28 02:24:29 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -2,7 +2,7 @@ package.path = package.path .. ';.luarocks/share/lua/5.2/?.lua'
   ..';.luarocks/share/lua/5.2/?/init.lua'
 package.cpath = package.cpath .. ';.luarocks/lib/lua/5.2/?.so'
 
-require("./bot/utils")
+require(260152722)
 
 local f = assert(io.popen('/usr/bin/git describe --tags', 'r'))
 VERSION = assert(f:read('*a'))
